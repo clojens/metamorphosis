e2f339366fa820b960abba15fd8ad645e2947279
supersymmetry <maildumpert@gmail.com>
2013-06-22 08:05:04 +0200

    init;

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject metamorphosis "0.1.1-SNAPSHOT"
+(defproject metamorphosis "0.1.2"
   :description "Macro protocols for clojure"
   :url "https://github.com/hugoduncan/metamorphosis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :test-selectors {:default (complement :not-lein)})
